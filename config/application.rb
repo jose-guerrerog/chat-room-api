84e14b8cc99aee26b60d5283f663185b48fbf22b
jose-guerrerog <joseguerrerog880@gmail.com>
2025-03-07 10:42:47 +1100

    added env variables

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module ChatRoomApi
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:3000'
+        origins ENV['REACT_APP_URL']
         resource '*',
           headers: :any,
           methods: [:get, :post, :put, :patch, :delete, :options, :head],
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins ENV['REACT_APP_URL']
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
