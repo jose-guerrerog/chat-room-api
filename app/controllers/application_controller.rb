d9bede48805fc9dc111180cf152fa59ea9eb6fff
jose-guerrerog <joseguerrerog880@gmail.com>
2025-03-08 17:55:42 +1100

    added more logs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   skip_before_action :verify_authenticity_token
   allow_browser versions: :modern
+
+  def test_broadcast
+    room_id = params[:room_id]
+    message = {
+      content: "Test message from server #{Time.now}",
+      sender_name: "Server",
+      timestamp: Time.now.to_i
+    }
+    ActionCable.server.broadcast("room_#{room_id}", message)
+    render plain: "Broadcast sent to room_#{room_id}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   get '/health', to: proc { [200, {}, ['ok']] }
 
   get '/debug/routes', to: 'debug#routes'
-  
+
+  get '/test_broadcast/:room_id', to: 'application#test_broadcast'
+
   resources :rooms, only: [:index, :create, :show] do
     resources :messages, only: [:create, :index]
   end
